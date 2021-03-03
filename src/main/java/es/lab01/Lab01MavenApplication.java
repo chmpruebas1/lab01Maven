b14de5ec3632f2dbfd4de55d345f3f9b8c8eea8d
INECO\jose.ruiz <jose.ruiz>
2021-03-03 11:43:30 +0100

    primer commit

---
+++ src/main/java/es/lab01/Lab01MavenApplication.java
@@ -0,0 +1,13 @@
+package es.lab01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab01MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab01MavenApplication.class, args);
+	}
+
+}
